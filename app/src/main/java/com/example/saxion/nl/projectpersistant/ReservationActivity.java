df74466284c27b01fb7c4b90c8c663e4c52ed05f
Ruben Assink <rubenassink@gmail.com>
2016-09-20 11:17:06 +0200

    reservation setup

---
+++ app/src/main/java/com/example/saxion/nl/projectpersistant/ReservationActivity.java
@@ -0,0 +1,19 @@
+package com.example.saxion.nl.projectpersistant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rubenassink on 20-09-16.
+ */
+public class ReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/saxion/nl/projectpersistant/model/Reservation.java
@@ -0,0 +1,12 @@
+package com.example.saxion.nl.projectpersistant.model;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ * Created by rubenassink on 20-09-16.
+ */
+public class Reservation {
+
+
+}
