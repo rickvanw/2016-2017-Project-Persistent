3c2106901b662c88ccc2ef0f220abcd0bcd6f54a
stanrothstegge <stanrothstegge@gmail.com>
2016-09-28 16:06:06 +0200

    eerste versie overzicht

---
+++ app/src/main/java/com/example/saxion/nl/projectpersistant/TimelineActivity.java
@@ -0,0 +1,81 @@
+package com.example.saxion.nl.projectpersistant;
+
+
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by HP-HP on 05-12-2015.
+ */
+public class TimeLineActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+
+    private TimeLineAdapter mTimeLineAdapter;
+
+    private List<TimeLineModel> mDataList = new ArrayList<>();
+
+    private GradientDrawable.Orientation mOrientation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+   //     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+  //      getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            setTitle("Vertical TimeLine");
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(getLinearLayoutManager());
+        mRecyclerView.setHasFixedSize(true);
+
+
+        initView();
+    }
+
+    private LinearLayoutManager getLinearLayoutManager() {
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+            return linearLayoutManager;
+
+    }
+
+    private void initView() {
+
+        for(int i = 0;i <20;i++) {
+            TimeLineModel model = new TimeLineModel();
+            model.setName("Random"+i);
+            model.setAge(i);
+            mDataList.add(model);
+        }
+
+        mTimeLineAdapter = new TimeLineAdapter(mDataList, mOrientation);
+        mRecyclerView.setAdapter(mTimeLineAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //Menu
+        switch (item.getItemId()) {
+            //When home is clicked
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+
+            default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
--- app/src/main/java/com/example/saxion/nl/projectpersistant/fragments/AgendaFragment.java
+++ app/src/main/java/com/example/saxion/nl/projectpersistant/fragments/AgendaFragment.java
@@ -1,18 +1,34 @@
 package com.example.saxion.nl.projectpersistant.fragments;
 
 import android.app.Fragment;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.saxion.nl.projectpersistant.R;
+import com.example.saxion.nl.projectpersistant.TimeLineAdapter;
+import com.example.saxion.nl.projectpersistant.TimeLineModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Gebruiker on 20-9-2016.
  */
 public class AgendaFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+
+    private TimeLineAdapter mTimeLineAdapter;
+
+    private List<TimeLineModel> mDataList = new ArrayList<>();
+
+    private GradientDrawable.Orientation mOrientation;
+
     public AgendaFragment() {
     }
 
@@ -20,6 +36,34 @@ public class AgendaFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.agenda_fragment, container, false);
+
+        mRecyclerView = (RecyclerView) rootview.findViewById(R.id.recyclerView);
+        mRecyclerView.setLayoutManager(getLinearLayoutManager());
+        mRecyclerView.setHasFixedSize(true);
+
+
+        initView();
+
         return  rootview;
+
+    }
+
+    private LinearLayoutManager getLinearLayoutManager() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        return linearLayoutManager;
+
+    }
+
+    private void initView() {
+
+        for(int i = 0;i <20;i++) {
+            TimeLineModel model = new TimeLineModel();
+            model.setName("Random"+i);
+            model.setAge(i);
+            mDataList.add(model);
+        }
+
+        mTimeLineAdapter = new TimeLineAdapter(mDataList, mOrientation);
+        mRecyclerView.setAdapter(mTimeLineAdapter);
     }
 }
