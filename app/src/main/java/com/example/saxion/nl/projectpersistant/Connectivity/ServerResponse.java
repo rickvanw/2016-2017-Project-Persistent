ef3662c38c6c7e2e3b1ff123082115146de7b932
stanrothstegge <stanrothstegge@gmail.com>
2016-10-13 12:20:17 +0200

    signIn activity

---
+++ app/src/main/java/com/example/saxion/nl/projectpersistant/Connectivity/ServerResponse.java
@@ -0,0 +1,10 @@
+package com.example.saxion.nl.projectpersistant.Connectivity;
+
+/**
+ * Enums voor verschillende HTTP response codes.
+ * Created by Stan_Rothstegge on 6-10-2016.
+ */
+public enum ServerResponse {
+    BAD_REQUEST, AUTHORISATION_REQUIRED, FORBIDDEN, NOT_FOUND, REQUEST_TIMEOUT,
+    INTERNAL_SERVER_ERROR, SERVICE_TEMPORARILY_UNAVAILABLE,
+}
--- app/src/main/java/com/example/saxion/nl/projectpersistant/SignIn.java
+++ app/src/main/java/com/example/saxion/nl/projectpersistant/SignIn.java
@@ -5,23 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-import com.wang.avi.AVLoadingIndicatorView;
+public class SignIn extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-private AVLoadingIndicatorView avi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
     }
+
 
     public void launchLogin(View v) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
-    }
-
-    void startAnim(){
-        avi.show();
-        // or avi.smoothToShow();
     }
 }
