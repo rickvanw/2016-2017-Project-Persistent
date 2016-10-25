bbb9dc2d241fde5ea8515eaa81e4dafca07667e9
stanrothstegge <stanrothstegge@gmail.com>
2016-10-25 20:36:29 +0200

    xml update + responsive beginscherm

--- app/src/main/java/com/example/saxion/nl/projectpersistant/MainActivity.java
+++ app/src/main/java/com/example/saxion/nl/projectpersistant/MainActivity.java
@@ -8,20 +8,46 @@ import android.view.View;
 import com.wang.avi.AVLoadingIndicatorView;
 
 public class MainActivity extends AppCompatActivity {
-private AVLoadingIndicatorView avi;
+    private AVLoadingIndicatorView avi;
+    private boolean stillLoading = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
+//        startAnim();
+//        while(stillLoading){
+//
+//        }
     public void launchLogin(View v) {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 
-    void startAnim(){
-        avi.show();
-        // or avi.smoothToShow();
+    private void startAnim(){
+   //     avi.show();
+    }
+//
+//    private void foutMetServer(){
+//        new BottomDialog.Builder(this)
+//                .setTitle("serverfout!")
+//                .setContent("App kan server niet bereiken. Probeer het later opnieuw")
+//                .setPositiveText("OK")
+//                .setPositiveBackgroundColorResource(R.color.colorPrimary)
+//                //.setPositiveBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary)
+//                .setPositiveTextColorResource(android.R.color.white)
+//                //.setPositiveTextColor(ContextCompat.getColor(this, android.R.color.colorPrimary)
+//                .onPositive(new BottomDialog.ButtonCallback() {
+//                    @Override
+//                    public void onClick(BottomDialog dialog) {
+//                        Log.d("BottomDialogs", "Do something!");
+//                    }
+//                }).show();
+//    }
+
+
+
+    private void hasInternetAcces(){
+
     }
 }
