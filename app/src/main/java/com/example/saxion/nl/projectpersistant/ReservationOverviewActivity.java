740d28c46007c5f3a71da4d45eb3d1aa32942745
Ruben Assink <rubenassink@gmail.com>
2016-10-27 03:36:59 +0200

    reservation delete

--- app/src/main/java/com/example/saxion/nl/projectpersistant/ReservationOverviewActivity.java
+++ app/src/main/java/com/example/saxion/nl/projectpersistant/ReservationOverviewActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -124,5 +125,17 @@ public class ReservationOverviewActivity extends AppCompatActivity {
                     }
                 })
                 .show();
+    }
+
+    public void OnRestart(){
+        reservationInOverviewAdapter.notifyDataSetChanged();
+
+        if (Build.VERSION.SDK_INT >= 11) {
+            recreate();
+        } else {
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        }
     }
 }
